1c1b89913befdc04a1654a50d1b6d4d1e719b732
Marco Scabbiolo <marco.scabbiolo@redb.ee>
2022-07-07 17:16:54 -0300

    feat(api): users service auth

--- api/sports/src/modules/user/domain/user.ts
+++ api/sports/src/modules/user/domain/user.ts
@@ -7,4 +7,5 @@ export interface User {
   email: string
   age: number
   image: Image
+  passowrd: string
 }
--- api/sports/src/modules/user/services/user.service.ts
+++ api/sports/src/modules/user/services/user.service.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { Injectable } from '@nestjs/common'
 import { User } from '../domain'
+import { v4 as uuid } from 'uuid'
 
 const INITIAL_LENGTH = 5
 const IMAGE_SIZE = 200
@@ -17,10 +18,35 @@ export class UserService {
       image: {
         url: faker.image.people(IMAGE_SIZE, IMAGE_SIZE),
       },
+      passowrd: '123456',
     }),
   )
+  private tokens = new Map<string, User>()
 
   getAll(): Promise<User[]> {
     return Promise.resolve(this.users)
+  }
+
+  async authenticateUser(
+    email: string,
+    password: string,
+  ): Promise<string | undefined> {
+    const user = this.users.find(
+      (u) => u.email === email && u.passowrd === password,
+    )
+
+    if (!user) {
+      return
+    }
+
+    const token = uuid()
+
+    this.tokens.set(token, user)
+
+    return token
+  }
+
+  async validateToken(token: string): Promise<User | undefined> {
+    return this.tokens.get(token)
   }
 }
